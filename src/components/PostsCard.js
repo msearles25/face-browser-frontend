f96d2d1f2d671ef0a9349742ac76d2ecff441de8
Michael <skaman4459@gmail.com>
2020-07-24 20:07:18 -0400

    homepage coming together

--- src/components/PostsCard.js
+++ src/components/PostsCard.js
@@ -2,18 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CardWrapper = styled.div`
+    position: relative;
     background: #fff;
-    width: 50%;
+    width: 100%;
     height: 150px;
     border-bottom: 1px solid lightgray;
     box-shadow: .1rem .1rem .3rem .1rem rgba(0,0,0,.1);
+    box-sizing: border-box;
     margin-bottom: 1rem;
 `;
 
-const PostsCard = ({ children }) => {
+const PostsCard = ({ post }) => {
     return (
         <CardWrapper>
-            {children}
+            {post.userHandle} <br />
+            {post.postContent} <br />
+            {post.createdOn} <br />
         </CardWrapper>
     )
 }
