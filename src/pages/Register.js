38d064d03636fbe44d27d1d952c5ff7c656e1402
Michael <skaman4459@gmail.com>
2020-07-25 11:28:32 -0400

    registering to the endpoint working

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,30 +1,56 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 
 const Register = () => {
-    const [imgSrc, setImgSrc] = useState();
-    const [prevImg, setPrevImg] = useState();
-
+    const [imgInfo, setImgInfo] = useState({
+        imgSrc: null,
+        prevImg: null
+    });
+    const [newUser, setNewUser] = useState()
+    
     const handleImageUpload = e => {
         if(e.target.files.length === 0) {
-            e.target.files = prevImg;
+            e.target.files = imgInfo.prevImg;
             return;
         }
-        if(e.target.files) {
-            const file = e.target.files
-            setPrevImg(file)
-        }
-
+        
+        const file = e.target.files
         const imgUrl = URL.createObjectURL(e.target.files[0]);
-        setImgSrc(imgUrl);
+        setImgInfo({ 
+            imgSrc: imgUrl, 
+            prevImg: file
+        });
+    }
+
+    const handleChange = e => {
+        setNewUser({
+            ...newUser,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post('http://localhost:1337/api/auth/register', newUser)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => {
+                console.log(error.response.data)
+            })
     }
 
     return (
         <div>
-            <form>
-                <input type='file' onChange={handleImageUpload}/>
-
+            <form onSubmit={handleSubmit}>
+                {/* <input type='file' onChange={handleImageUpload}/> */}
+                <input type='text' name='userHandle' placeholder='Handle' onChange={handleChange}/>
+                <input type='text' name='email' placeholder='Email' onChange={handleChange}/>
+                <input type='text' name='password' placeholder='Password' onChange={handleChange}/>
+                <input type='text' name='confirmPassword' placeholder='Confirm Password' onChange={handleChange}/>
+                <button type='submit'>test register</button>
             </form>
-            {imgSrc && <img src={imgSrc} alt='user'/> }
+            {imgInfo.imgSrc && <img src={imgInfo.imgSrc} alt='user'/> }
         </div>
     )
 }
