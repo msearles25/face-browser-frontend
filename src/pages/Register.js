6d0e662f00988fa9fc3c46b21bccddf25dd39598
Michael <skaman4459@gmail.com>
2020-07-25 12:45:54 -0400

    image upload partially woring

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,13 +4,13 @@ import axios from 'axios';
 const Register = () => {
     const [imgInfo, setImgInfo] = useState({
         imgSrc: null,
-        prevImg: null
+        imgFile: null
     });
     const [newUser, setNewUser] = useState()
     
     const handleImageUpload = e => {
         if(e.target.files.length === 0) {
-            e.target.files = imgInfo.prevImg;
+            e.target.files = imgInfo.imgFile;
             return;
         }
         
@@ -18,7 +18,7 @@ const Register = () => {
         const imgUrl = URL.createObjectURL(e.target.files[0]);
         setImgInfo({ 
             imgSrc: imgUrl, 
-            prevImg: file
+            imgFile: file
         });
     }
 
@@ -31,6 +31,7 @@ const Register = () => {
 
     const handleSubmit = e => {
         e.preventDefault();
+       
         axios.post('http://localhost:1337/api/auth/register', newUser)
             .then(res => {
                 console.log(res)
@@ -40,10 +41,26 @@ const Register = () => {
             })
     }
 
+    const testImage = e => {
+        e.preventDefault()
+
+        const imageData = new FormData();
+        const image = imgInfo.imgFile[0]
+        imageData.append('upload_preset', `${process.env.REACT_APP_UPLOAD_PRESET}`)
+        imageData.append('file', image);
+        axios.post(`${process.env.REACT_APP_IMAGE_UPLOAD_URL}`, imageData)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(error => {
+            console.log(error.response)
+        })
+    }
+
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                {/* <input type='file' onChange={handleImageUpload}/> */}
+            <form onSubmit={testImage}>
+                <input type='file' onChange={handleImageUpload}/>
                 <input type='text' name='userHandle' placeholder='Handle' onChange={handleChange}/>
                 <input type='text' name='email' placeholder='Email' onChange={handleChange}/>
                 <input type='text' name='password' placeholder='Password' onChange={handleChange}/>
