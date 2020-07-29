194aa20fbadaca2df660497be7e48b732978c1b8
Michael <skaman4459@gmail.com>
2020-07-28 23:15:02 -0400

    auth setup along with a check to log user out if token is expired

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 import axiosWithAuth from '../../components/auth/axiosWithAuth';
 import { setErrors, clearAllErrors } from './uiActions';
-import { SET_USER, LOADING_UI, SET_AUTHENTICATED } from '../types';
+import { SET_USER, LOADING_UI, SET_AUTHENTICATED, SET_UNAUTHENTICATED } from '../types';
 
 export const getUserInfo = () => async dispatch => {
-
     try {
         const response = await axiosWithAuth().get('/user');
         const userInfo = await response.data
-        console.log(userInfo)
         dispatch({ type:SET_USER, payload: userInfo});
     }
     catch(error) {
         console.log(error)
     }
 }
-
+export const authUser = () => dispatch => {
+    dispatch({ type:SET_AUTHENTICATED });
+}
 export const loginUser = (user, history) => async dispatch => {
     dispatch({ type:LOADING_UI });
     try {
         const userToken = await axios.post('http://localhost:1337/api/auth/login', user);
-        localStorage.setItem('token', await userToken.data.token)   
+        localStorage.setItem('token', userToken.data.token)   
         dispatch(getUserInfo());
-        dispatch({ type:SET_AUTHENTICATED })
+        dispatch(authUser())
         dispatch(clearAllErrors());
         history.push('/')
     }
@@ -31,16 +31,19 @@ export const loginUser = (user, history) => async dispatch => {
         dispatch(setErrors(error.response.data))
     }
 }
-
+export const logoutUser = () => dispatch => {
+    localStorage.removeItem('token');
+    dispatch({ type:SET_UNAUTHENTICATED });
+}
 export const registerUser = (newUser, imageUpload, imgInfo, history) => async dispatch => {
     dispatch({ type:LOADING_UI })
     try {
         const user = await axios.post('http://localhost:1337/api/auth/register', newUser);
         const userImage = await imageUpload(imgInfo);
         const { token } = await user.data;
-        localStorage.setItem('token', await token)
+        localStorage.setItem('token', token)
         await axiosWithAuth().put(`/user`,{
-            imageUrl: await userImage
+            imageUrl: userImage
         })
         dispatch(clearAllErrors())
         history.push('/')
