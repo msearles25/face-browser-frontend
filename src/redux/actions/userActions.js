345d94a9186cdb2867e2e0938cd6ea6bf1a682bc
Michael <skaman4459@gmail.com>
2020-08-03 16:59:57 -0400

    changed urls to deployed backend

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -22,7 +22,7 @@ export const authUser = () => dispatch => {
 export const loginUser = (user, history) => async dispatch => {
     dispatch({ type:LOADING_UI });
     try {
-        const userToken = await axios.post('http://localhost:1337/api/auth/login', user);
+        const userToken = await axios.post('https://face-browser.herokuapp.com/api/auth/login', user);
         localStorage.setItem('token', userToken.data.token)   
         dispatch(getUserInfo());
         dispatch(authUser());
@@ -44,7 +44,7 @@ export const logoutUser = history => dispatch => {
 export const registerUser = (newUser, imageUpload, imgInfo, history) => async dispatch => {
     dispatch({ type:LOADING_UI })
     try {
-        const user = await axios.post('http://localhost:1337/api/auth/register', newUser);
+        const user = await axios.post('https://face-browser.herokuapp.com/api/auth/register', newUser);
         const userImage = await imageUpload(imgInfo);
         const { token } = await user.data;
         localStorage.setItem('token', token)
